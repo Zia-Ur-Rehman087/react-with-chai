2698e7f966eb3959b876da463191d197d7daf9e3
Zia-Ur-Rehman087 <161125181+Zia-Ur-Rehman087@users.noreply.github.com>
2024-02-28 10:10:08 +0000

    viteChangeingd saved

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,9 +1,16 @@
 
 function customRender(reactElement, container){
+// const domElement = document.createElement(reactElement.type);
+// domElement.innerHTML = reactElement.children;
+// domElement.setAttribute("href",reactElement.props.href);
+// domElement.setAttribute("target",reactElement.props.target);
+// container.appendChild(domElement);
 const domElement = document.createElement(reactElement.type);
 domElement.innerHTML = reactElement.children;
-domElement.setAttribute("href",reactElement.props.href);
-domElement.setAttribute("target",reactElement.props.target);
+for(const prop in reactElement.props){
+    if(prop === "cildren") continue
+    domElement.setAttribute(prop , reactElement.props[prop])
+}
 container.appendChild(domElement);
 }
 const reactElement = {

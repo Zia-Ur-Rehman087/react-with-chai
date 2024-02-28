ec6271caf3c3ea4ac83e4be051940a47351ceae7
Zia-Ur-Rehman087 <161125181+Zia-Ur-Rehman087@users.noreply.github.com>
2024-02-28 08:16:49 +0000

    files

---
+++ customreact/customreact.js
@@ -0,0 +1,19 @@
+
+function customRender(reactElement, container){
+const domElement = document.createElement(reactElement.type);
+domElement.innerHTML = reactElement.children;
+domElement.setAttribute("href",reactElement.props.href);
+domElement.setAttribute("target",reactElement.props.target);
+domElement.appendChild(container);
+}
+const reactElement = {
+    type : "a",
+    props:{
+        href:"https://google.com",
+        target:'-blank'
+    },
+    children:"Click me to visit google"
+}
+
+const mainContainer = document.querySelector("#root");
+customRender(reactElement, mainContainer)
